f7f16a3632a0b20cbdb39c1797f0027a7875cdb5
Ezepue <ebukaezepue@gmail.com>
2023-09-16 12:35:05 +0100

    push

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _printf - a function that produces output according to a format
+ * @format: character string composed of zero or moe directives
+ * Return: the number of characters printed (excluding null byte)
+ */
+
+int _printf(const char *format, ...);
+
+
+
---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdarg.h>
+#include <unstd.h>
+
+#define BUFF_SIZE 1024
+#define UNUSED (x) (void) (x)
+
+int _printf(const char *format, ...);
+
+#endif
